9e54e4b13e6d089be3e755b765588a657f1aa850
anmstha123 <61106764+anmstha123@users.noreply.github.com>
2022-07-03 15:57:13 -0500

    Added new homepage and check for login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,33 +1,31 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import SignupModal from "./SignupModal";
 import "../Style/styles.css";
-import { Modal } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+export let database = [
+  {
+    username: "name",
+    password: "password"
+  },
+  {
+    username: "anmstha123@gmail.com",
+    password: "password1"
+  }
+];
 
 const Login = () =>  {
     // React States
     const [errorMessages, setErrorMessages] = useState({});
     const [isSubmitted, setIsSubmitted] = useState(false);
-  
+    const [modalShow, setModalShow] = useState(false);
+    let history = useHistory();
     // User Login info
-    const database = [
-      {
-        username: "name",
-        password: "password"
-      },
-      {
-        username: "name1",
-        password: "password1"
-      }
-    ];
-  
     const errors = {
       uname: "invalid username",
       pass: "invalid password"
     };
-  
     const handleSubmit = (event) => {
       //Prevent page reload
       event.preventDefault();
@@ -44,6 +42,7 @@ const Login = () =>  {
           setErrorMessages({ name: "pass", message: errors.pass });
         } else {
           setIsSubmitted(true);
+          localStorage.setItem("isSubmitted", true);
         }
       } else {
         // Username not found
@@ -57,54 +56,7 @@ const Login = () =>  {
         <div className="error">{errorMessages.message}</div>
       );
 
-      const MyVerticallyCenteredModal=(props) =>{
-        return (
-          <Modal
-            {...props}
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-          >
-            <Modal.Header closeButton>
-              <Modal.Title id="contained-modal-title-vcenter">
-                Sign Up
-                <h6>Quick and easy</h6>
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-            <form>
-            <div class="input-group">
-              <input type="text" className="form-control" placeholder="First name"/>
-              <input type="text" className="form-control" placeholder="Last name" />
-            </div>
-            <div className="mb-3">
-            </div>
-            <div className="mb-3">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Email"
-              />
-            </div>
-            <div className="mb-3">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Enter password"
-              />
-            </div>
-            <div className="d-grid">
-              <button type="submit" className="btn btn-primary" style={{background:"#2c9ad9", border:"#2c9ad9"}}>
-                Sign Up
-              </button>
-            </div>
-            <p className="forgot-password text-right">
-              Already registered <a href="/login">sign in?</a>
-            </p>
-          </form>
-            </Modal.Body>
-          </Modal>
-        );
-      }
+
   
     // JSX code for login form
     const renderForm = (
@@ -112,7 +64,7 @@ const Login = () =>  {
         <form onSubmit={handleSubmit}>
           <div className="input-container">
             <label>Username </label>
-            <input type="text" name="uname" required />
+            <input type="email" name="uname" required />
             {renderErrorMessage("uname")}
           </div>
           <div className="input-container">
@@ -120,20 +72,18 @@ const Login = () =>  {
             <input type="password" name="pass" required />
             {renderErrorMessage("pass")}
           </div>
-          <Button as="input" type="submit" value="Login" />{' '}
+          <Button variant="primary" as="input" type="submit" value="Login" />{' '}
           <div className="forgotPassword">
-            <a href="aaa.html">Forgot Password?</a>
+            <a href="/forgotpassword">Forgot Password?</a>
           </div>
           <br/>
           <div class="col-md-12 text-center">
-            <Button variant="primary" style={{background:"#2c9ad9", border:"#2c9ad9"}} onClick={() => setModalShow(true)}>Sign Up</Button>
+            <Button variant="success" className="signUp" onClick={() => setModalShow(true)}>Sign Up</Button>
           </div>
         </form>
       </div>
     );
     
-    let history = useHistory();
-    const [modalShow, setModalShow] = React.useState(false);
     return (
         <div className="app">
             <div className="slogan">
@@ -143,7 +93,7 @@ const Login = () =>  {
             <div className="login-form">
                 {isSubmitted ? history.push("/homepage"): renderForm}
             </div>
-            <MyVerticallyCenteredModal show={modalShow} onHide={() => setModalShow(false)}/>
+            <SignupModal show={modalShow} onHide={() => setModalShow(false)}/>
         </div>
     );
 }
