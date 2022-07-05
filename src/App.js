0496c1abeba77cceb96674c562252dd53081e94f
anmstha123 <61106764+anmstha123@users.noreply.github.com>
2022-07-05 18:33:19 -0500

    Added express and started to add database

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ import Homepage from "./Pages/Homepage";
 import ForgotPassword from "./Pages/ForgotPassword";
 
 function App() {
+  const getData = async (url)=>{
+    const nreData = await fetch(url,{
+      method: 'GET',
+      headers:{
+        'content-type':'application/jason',
+        'Accept': 'application/jason'
+      }
+    })
+  }
+
+
   return(
     <Router>
       <Switch>
-        <Route exact path="/login" component = {Login}/>
+        <Route exact path="/" component = {Login}/>
         <Route exact path="/homepage" component = {Homepage}/>
         <Route exact path="/forgotpassword" component = {ForgotPassword}/>
       </Switch>
