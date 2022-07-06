8ba7eebc706b926e03d3cdf2c38de4a591feb4c7
Bhuwan <bhuwan@Admins-MacBook-Pro.local>
2022-07-05 22:38:54 -0500

    navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,19 +5,16 @@ import AuthApi from '../../apis/auth_api/auth_api'
 function Navbar({ currentUser, logout }) {
     return (
         <nav className="navbar sticky-top navbar-expand navbar-dark bg-primary">
-            <Link to={"/"} style={{ marginLeft: "10px" }} className="navbar-brand">
+            <Link to={"/"} style={{ marginLeft: "10px" }} className="navbar-brand me-auto">
                 SmartPlacement
             </Link>
-
-            <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                    <Link to={"/home"} className="nav-link">
-                        Home
-                    </Link>
-                </li>
-            </ul>
             {currentUser ? (
-                <ul className="navbar-nav ml-auto">
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item">
+                        <Link to={"/home"} className="nav-link">
+                            Home
+                        </Link>
+                    </li>
                     <li className="nav-item">
                         <Link to={"/home"} className="nav-link">
                             Status
@@ -46,12 +43,12 @@ function Navbar({ currentUser, logout }) {
                     </li>
                     <li className="nav-item">
                         <a href="/" className="nav-link" onClick={logout}>
-                            LogOut
+                            Logout
                         </a>
                     </li>
                 </ul>
             ) : (
-                <ul className="navbar-nav ml-auto">
+                <ul className="navbar-nav me-auto">
                     <li className="nav-item">
                         <Link to={"/"} className="nav-link">
                             Login
